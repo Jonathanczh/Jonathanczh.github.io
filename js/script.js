c495203c8ff72d0e4dfc4ccecb95c68cd313992b
Jonathan Chiu <150285354+Jonathanczh@users.noreply.github.com>
2024-04-12 00:57:42 +0800

    new

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('a[href^="#"]');
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function(event) {
+            event.preventDefault();
+            var target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
