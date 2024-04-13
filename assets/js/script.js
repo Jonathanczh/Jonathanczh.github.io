f8c69a89a7cec8e5279e946fa428c7ce37a7785c
Jonathanczh <jonathanczh00@gmail.com>
2024-04-14 05:23:56 +0800

    modified abit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // script.js
 function scrollToProjects() {
+    console.log("Function called");  // This should appear in the console when you click the arrow
     const projectsSection = document.getElementById('projects');
     projectsSection.scrollIntoView({ behavior: 'smooth' });
 }
